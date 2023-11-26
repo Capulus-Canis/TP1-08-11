71413494f8268a7241cc41e57ce270543d2d84db
Capulus-Canis <143612912+Capulus-Canis@users.noreply.github.com>
2023-11-26 19:50:39 -0300

    Add files via upload

--- servidor.js
+++ servidor.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const app = express ()
+const port = 8080
+const express = require("express");
+const app = express();
 app.set("view engine", "ejs")
 
-app.get("/JF", (request, response) => {
-    response.render('JF')
-})
-app.get("/barra", (request, response) => {
-    response.render('barra')
-})
-app.get("/visgueiro", (request, response) => {
-    response.render('visgueiro')
-})
-app.get("/", (request, response) => {
-    response.render('inicio')
-})
+app.use('/public', express.static('public'));
 
-app.listen(8080)
+app.get("/", (requisicao, resposta) => {
+    resposta.render('index')
+})
+app.get("/barra", (requisicao, resposta) => {
+    resposta.render('barra')
+})
+app.get("/JF", (requisicao,resposta) => {
+    resposta.render('JF')
+})
+app.get("/passagem", (requisicao,resposta) => {
+    resposta.render('passagem')
+})
+app.get("/visgueiro", (requisicao,resposta) => {
+    resposta.render('visgueiro')
+})
+console.log("servidor funcionando na porta:", port)
+app.listen(port)
