7747c0d4b32d75f2387be49ea156733e7c895178
Capulus-Canis <143612912+Capulus-Canis@users.noreply.github.com>
2023-11-27 09:45:05 -0300

    Add files via upload

--- servidor.js
+++ servidor.js
@@ -1,8 +1,11 @@
 const port = 8080
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 app.set("view engine", "ejs")
-
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use('/public', express.static('public'));
 
 app.get("/", (requisicao, resposta) => {
@@ -19,6 +22,16 @@ app.get("/passagem", (requisicao,resposta) => {
 })
 app.get("/visgueiro", (requisicao,resposta) => {
     resposta.render('visgueiro')
+})
+
+app.post('/salvar', (req, res) => {
+    let nomeuser = req.body.nm
+   
+    let cadastro = {nm: nomeuser}
+    let fileData = JSON.parse(fs.readFileSync(`users.json`))
+    fileData.push(cadastro)
+    resultado = fs.writeFileSync(`users.json`, JSON.stringify(fileData))
+    res.render('index', { resultado })
 })
 console.log("servidor funcionando na porta:", port)
 app.listen(port)
